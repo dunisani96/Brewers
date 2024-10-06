01463a1779b25de4f14581928a19029a2eab8b0c
Dunisani Makondo <dunisani7@gmail.com>
2024-10-06 08:25:23 +0200

    Initial Commit

---
+++ Brewers/BrewersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewersApp.swift
+//  Brewers
+//
+//  Created by Dunisani Makondo on 2024/10/06.
+//
+
+import SwiftUI
+
+@main
+struct BrewersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrewersTests/BrewersTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrewersTests.swift
+//  BrewersTests
+//
+//  Created by Dunisani Makondo on 2024/10/06.
+//
+
+import Testing
+@testable import Brewers
+
+struct BrewersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
